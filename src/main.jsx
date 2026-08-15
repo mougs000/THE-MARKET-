a56f5923209ce4f5cd60659021addc6c8803c335
mougs000 <kingmorgazi1@gmail.com>
2026-08-15 13:54:45 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./lib/storagePolyfill.js";
+import "./index.css";
+import TheMarket from "./App.jsx";
 
-function showError(err) {
-  const root = document.getElementById("root");
-  root.innerHTML =
-    '<pre style="color:red;font-family:monospace;font-size:12px;white-space:pre-wrap;padding:16px;">' +
-    String(err && err.stack ? err.stack : err) +
-    "</pre>";
-}
-
-async function start() {
-  try {
-    await import("./lib/storagePolyfill.js");
-    await import("./index.css");
-    const { default: TheMarket } = await import("./App.jsx");
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <React.StrictMode>
-        <TheMarket />
-      </React.StrictMode>
-    );
-  } catch (err) {
-    showError(err);
-  }
-}
-
-start();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TheMarket />
+  </React.StrictMode>
+);
